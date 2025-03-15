8465864987a7ff907ef3a4633bca1ea8d7a9a3e5
ReiRei1905 <rttorio@student.apc.edu.ph>
2025-03-15 22:02:53 +0800

    added admin signup page
    not yet finished but comitting
    need to fix the proper formatting based off the ui

---
+++ user_info/admin_pop-up.js
@@ -0,0 +1,11 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function submitForm() {
+    document.getElementById('admin-signup-form').submit();
+}
